b962a21934528d74085843dd8f3b1c1a9890fdea
marsel-b-games <marselbkak@mail.ru>
2023-10-09 20:05:27 +0500

    Add Resources folder
    - Переместил все ScriptableObjects в Resources

--- Assets/Scripts/Main/CardDisplay.cs
+++ Assets/Scripts/Main/CardDisplay.cs
@@ -36,7 +36,7 @@ public class CardDisplay : MonoBehaviour
     // 		//cardRarity.text = card.cardRarity.ToString();
     // 		//HideInformation();
     // 	}
-    // Use this for initialization
+
     void Start()
     {
         nameText.text = card.name;
