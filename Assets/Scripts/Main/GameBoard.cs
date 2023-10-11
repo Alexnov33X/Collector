b1f3414df240d52cf19d283d7627ab09b6b74da8
A-Mironov <aamironov_1@edu.hse.ru>
2023-10-11 17:21:42 +0300

    Game Board is done?

--- Assets/Scripts/Main/GameBoard.cs
+++ Assets/Scripts/Main/GameBoard.cs
@@ -41,7 +41,22 @@ public class GameBoard : MonoBehaviour
             e.transform.position = cardSlots[i].position;
         }
     }
-    private void Start()
+    public void OrderAttack(bool isPlayer) //проходим по всем сущностям и говорим им атаковать
+    {
+        if (isPlayer)
+        {
+            for (int i = 0; i < playerSlots; i++)
+                if (occupants[i] != null)
+                    occupants[i].Attack(this, i);
+        }
+        else
+        {
+            for (int i = playerSlots; i < playerSlots + enemySlots; i++)
+                if (occupants[i] != null)
+                    occupants[i].Attack(this, i);
+        }
+    }
+    private void Start() // для тестов
     {
         Debug.Log(occupants[0]);
         Debug.Log(occupants[0] == null);
