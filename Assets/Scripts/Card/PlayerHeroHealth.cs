33c7386c525b660d5e0355280938005c2e49b669
marsel-b-games <marselbkak@mail.ru>
2023-10-25 17:52:39 +0500

    UI edited

---
+++ Assets/Scripts/Card/PlayerHeroHealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeroHealth : MonoBehaviour
+{
+    public PlayerHeroDisplay pd;
+
+    private int health = 90;
+    public int Health { get => health; set => health = value; }
+
+    void Start()
+    {
+        pd.updateInformation(health.ToString());
+    }
+
+    public void OnHit(int damage)
+    {
+        Health -= damage;
+        pd.updateInformation(Health.ToString());
+    }
+
+}
