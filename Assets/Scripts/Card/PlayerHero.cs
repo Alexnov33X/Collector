c7f837732b5e433e5166919fae28a6dd20d9a5c4
AlexWork <a.mironov@crumplestudios.com>
2023-10-25 16:09:45 +0300

    For merge

--- Assets/Scripts/Card/PlayerHero.cs
+++ Assets/Scripts/Card/PlayerHero.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHeroHealth : MonoBehaviour
+public class PlayerHero : MonoBehaviour
 {
     public PlayerHeroDisplay pd;
 
